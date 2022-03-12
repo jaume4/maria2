ef45ef64e6aacd1e85c7e46115976bd9746edbea
Jaume Corbi <jaume4@gmail.com>
2022-03-12 18:36:29 +0100

    Initial Commit

---
+++ Maria2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maria2
+//
+//  Created by Jaume on 12/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maria2/Maria2App.swift
@@ -0,0 +1,17 @@
+//
+//  Maria2App.swift
+//  Maria2
+//
+//  Created by Jaume on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Maria2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
